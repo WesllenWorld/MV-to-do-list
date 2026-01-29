01abbf91606602db838a97bc4871e86eaad1d57a
WesllenWorld <wesllen.2020111052@unicap.br>
2026-01-29 14:44:20 -0300

    chore: DbContext e entidade Todo

---
+++ MV-to-do-list/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MV_to_do_list.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
