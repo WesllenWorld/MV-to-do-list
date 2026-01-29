a62a5933fc191ba46458a386c243e3b3f60886f1
WesllenWorld <wesllen.2020111052@unicap.br>
2026-01-29 16:32:24 -0300

    Feat: Services de Post, Put e Delete ok

--- MV-to-do-list/Services/TodoService.cs
+++ MV-to-do-list/Services/TodoService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MV_to_do_list.Context;
+using MV_to_do_list.DTOs;
 using MV_to_do_list.Entities;
+using System;
 
 namespace MV_to_do_list.Services
 {
@@ -12,14 +14,57 @@ namespace MV_to_do_list.Services
         {
             _context = context;
         }
-        public async Task<IEnumerable<Todo>> GetTodosService()
+        public async Task<IEnumerable<Todo>> GetAllTodos()
         {
             return await _context.Todos.ToListAsync();
         }
 
-        public async Task<Todo> GetTodoByIdService(long Id)
+        public async Task<Todo> GetTodoById(long id)
         {
-            return await _context.Todos.FindAsync(Id);
+            return await _context.Todos.FindAsync(id);
+        }
+
+        public async Task<Todo> CreateTodo(CreateTodoDTO todoDTO)
+        {
+            Todo newTodo = new Todo
+            {
+                Title = todoDTO.Title,
+                Description = todoDTO.Description,
+                Status = todoDTO.Status
+            };
+
+            _context.AddAsync(newTodo);
+            await _context.SaveChangesAsync();
+
+            return newTodo;
+        }
+
+        public async Task<bool> UpdateTodoStatusById(long id, UpdateTodoStatusDTO updateTodo)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            //bool updated = true;
+
+            if(todo == null)
+            {
+                return false;
+            }
+            todo.Status = updateTodo.Status;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteTodoById(long id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            
+            if (todo == null)
+            {
+                return false;
+            }
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return true;
         }
     }
 }
